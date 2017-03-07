f007ea32052cd99f262b8829e315c94ac80e3fc7
simonguo <simonguo.2009@gmail.com>
2017-03-07 10:55:50 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const marked = require('marked');
+const hl = require('highlight.js');
+
+const renderer = new marked.Renderer();
+
+const codeRenderer = function (code, lang) {
+    lang = lang === 'js' ? 'javascript' : lang;
+    if (lang === 'html') {
+        lang = 'xml';
+    }
+
+    const hlCode = lang ? hl.highlight(lang, code).value : hl.highlightAuto(code).value;
+    return `<div class="doc-highlight"><pre><code class="${lang || ''}">${hlCode}</code></pre></div>`;
+};
+
+renderer.code = codeRenderer;
+
+module.exports = {
+    renderer: renderer
+};
