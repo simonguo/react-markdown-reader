3884a8f3d05e8edbdc1d66ee4b5f811382414c0a
simonguo <simon.guo@hypers.com>
2018-05-17 15:30:19 +0800

    configurable language

--- src/renderer.js
+++ src/renderer.js
@@ -1,18 +1,35 @@
-const marked = require('marked');
-const hl = require('highlight.js');
+const marked = require("marked");
+const hl = require("highlight.js/lib/highlight");
+const defalutLanguages = [
+  "javascript",
+  "bash",
+  "xml",
+  "css",
+  "markdown",
+  "less"
+];
 
-const renderer = new marked.Renderer();
+export default (languages = defalutLanguages) => {
+  languages.forEach(langName => {
+    let langModule = require(`highlight.js/lib/languages/${langName}`);
+    hl.registerLanguage(langName, langModule);
+  });
 
-const codeRenderer = function (code, lang) {
-  lang = lang === 'js' ? 'javascript' : lang;
-  if (lang === 'html') {
-    lang = 'xml';
-  }
+  const renderer = new marked.Renderer();
+  const codeRenderer = function(code, lang) {
+    lang = lang === "js" ? "javascript" : lang;
+    if (lang === "html") {
+      lang = "xml";
+    }
 
-  const hlCode = lang ? hl.highlight(lang, code).value : hl.highlightAuto(code).value;
-  return `<div class="doc-highlight"><pre><code class="${lang || ''}">${hlCode}</code></pre></div>`;
+    const hlCode = lang
+      ? hl.highlight(lang, code).value
+      : hl.highlightAuto(code).value;
+    return `<div class="doc-highlight"><pre><code class="${lang ||
+      ""}">${hlCode}</code></pre></div>`;
+  };
+
+  renderer.code = codeRenderer;
+
+  return renderer;
 };
-
-renderer.code = codeRenderer;
-
-export default renderer;
